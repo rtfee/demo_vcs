8bc80be04c05292141b9691401f3b92f364b64a7
Ryan <ryan.fee625@gmail.com>
2019-10-09 14:05:56 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  access_key = "${var.scalr_aws_access_key}"
+  secret_key = "${var.scalr_aws_secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "scalr" {
+  ami                    = "ami-2757f631"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-0ebb1058ad727cfdb"
+  vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
+  key_name               = "ryan"
+}
+
+terraform {
+  backend "remote" {
+    hostname = "my.scalr.com:443"
+    organization = "org-sgncvo4mr5l4na0"
+    workspaces {
+      name = "cli_test"
+    }
+  }
+}
